6326a4527ac5c088d866a171ba741c0977eac819
Soumitro Halder Shan <shan@up-seo.at>
2026-02-07 13:51:52 +0600

    Finalize production deployment: Consolidate App logic and cleanup debug logs

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { TestApp } from './TestApp'
-
-console.log("DEBUG: TestApp Imported");
+import App from './App'
 
 const root = document.getElementById('root');
 if (root) {
-    try {
-        console.log("DEBUG: Calling createRoot");
-        const reactRoot = ReactDOM.createRoot(root);
-        console.log("DEBUG: Calling render");
-        reactRoot.render(<TestApp />);
-        console.log("DEBUG: Render Call Finished");
-    } catch (e) {
-        console.error("DEBUG: Render Fatal Error", e);
-        root.innerHTML = `<div style="color: red;"><h1>FATAL RENDER ERROR</h1><pre>${e}</pre></div>`;
-    }
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
 
 
